b3138a29f95f3c15565424673cdc9f4ab17da6b3
Ait younes Mehdi Adel <overpex@gmail.com>
2014-02-24 08:24:00 +0100

    Test

---
+++ success_v1/src/com/success_v1/splash_screen/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.success_v1.splash_screen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.success_v1.main.Main;
+import com.success_v1.successCar.R;
+
+public class SplashActivity extends Activity {
+	
+	private static int SPLASH_TIME_OUT = 3000;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+		
+		getActionBar().setTitle(null);
+		getActionBar().setDisplayShowHomeEnabled(false);
+		
+		new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, Main.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+	}
+	
+	
+
+}
